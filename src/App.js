9a99a481896e381c17583916c115a7b96a63c814
Vaibhav Sharma <sharmavaibhav5796@gmail.com>
2021-02-11 17:44:41 +0530

    root file for components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import FindKey from "./components/FindKey";
+import PrimeNumber from "./components/PrimeNumber";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Login from "./components/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div className="App">
+            <NavBar />
+            <Switch>
+              <Route exact path="/" component={PrimeNumber} />
+              <Route path="/findkey" component={FindKey} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
